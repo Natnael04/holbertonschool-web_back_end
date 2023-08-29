c17a853f21b4ba79e95bbcc713277e3cc9436a48
Natnael <>
2023-08-29 14:09:02 +0200

    define and annotate variables

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-
+"""define and annotate variable with int value"""
 a: int = 1
-
+"""define and annotate variable with float value"""
 pi: float = 3.14
-
+"""define and annotate variable with boolean value"""
 i_understand_annotations: bool = True
-
+"""define and annotate variable with string value"""
 school: str = "Holberton"
