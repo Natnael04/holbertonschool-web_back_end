62fd4485892a75c35efc550fc58d090d040b274a
Natnael <>
2023-08-29 10:25:43 +0200

    fix the space

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""to do type-annotated for the function."""
+
 
 def add(a: float, b: float) -> float:
+    """to do type-annotated for the function."""
     sum = a + b
     return sum
