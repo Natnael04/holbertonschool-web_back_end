76f91879c35db72eb10af3e849e48fb888854710
Natnael <>
2023-08-29 13:44:33 +0200

    basic annotation to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""for return the string representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """a function to return the string representation"""
+    return str(n)
