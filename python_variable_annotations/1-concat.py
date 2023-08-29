d03fd0c69c31a78710c971e3a45ef1bf194d067c
Natnael <>
2023-08-29 10:58:21 +0200

    concate strings

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""to concatenate two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenate two strings and return."""
+    str3 = "str1, " + "str2!"
+    return str3
