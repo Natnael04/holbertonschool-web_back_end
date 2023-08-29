4986f93954d4bf3a27be8b1303ed42d4adb8a8d0
Natnael <>
2023-08-29 15:12:37 +0200

    list of floats

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """take a list of float as argument and return the sum"""
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """create the varible and  return sum of the list"""
     total = 0.0
     for num in input_list:
