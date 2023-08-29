16e78535f29456b4a8f791dcc568987377d1d41b
Natnael <>
2023-08-29 12:30:15 +0200

    basic annotation floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""for taking the float as an argument and return the floor of the float."""
+import math
+
+
+def floor(n: float) -> int:
+    """to calculate the floor value of the input"""
+    return math.floor(n)
