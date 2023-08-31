38a043264291bcb6ef7ab97b7c5e054088ca4caa
Natnael <>
2023-08-31 19:43:56 +0200

    first task

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Asynchronous coroutine that introduces a random delay and returns the delay duration.
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """Asynchronous coroutine that introduces a random delay and returns the delay duration.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
