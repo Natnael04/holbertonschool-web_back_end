fb5c6613e97736d5f0d2c8d99e7c68188d5ed090
Natnael <>
2023-08-31 20:04:43 +0200

    fix the space issue

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Asynchronous coroutine that introduces a random delay and returns the delay duration.
+"""Asynchronous coroutine that introduces a random delay and
+returns the delay duration.
 """
 
 import asyncio
@@ -7,7 +8,8 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Asynchronous coroutine that introduces a random delay and returns the delay duration.
+    """Asynchronous coroutine that introduces a random delay and
+    returns the delay duration.
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
